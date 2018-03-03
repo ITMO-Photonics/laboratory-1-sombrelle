5a8d5253aa2e1eaeb04c26c239d6fb7240039c57
sombrelle <36609019+sombrelle@users.noreply.github.com>
2018-03-03 11:14:14 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,10 @@
+import math
 
+R = int(input("Write R: "))
+L = int(input("Write L: "))
+
+def calculate (R, L):
+    cylinder_volume = math.pi * L * R * R
+    return ("cylinder_volume: " + str(cylinder_volume))
+
+print(calculate(R,L))
